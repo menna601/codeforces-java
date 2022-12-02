2694034c4f4fb0c2fec8fef5451b1c112252278c
Menna Emam <menna20101@gmail.com>
2022-12-02 19:25:57 +0200

    P-71A

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/4/A
+//https://codeforces.com/problemset/problem/71/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        byte weight = scanner.nextByte();
-        if(weight % 2 == 0 && weight != 2)
-            System.out.println("YES");
-        else
-            System.out.println("NO");
+        byte noOfWords = scanner.nextByte();
+        for (byte i = 0; i < noOfWords; i++) {
+            String word = scanner.next();
+            int wordLength = word.length();
+            char firstChar = word.charAt(0);
+            char lastChar = word.charAt(wordLength -1);
+            if(wordLength > 10 )
+                System.out.println("" + firstChar + (wordLength-2) + lastChar);
+            else
+                System.out.println(word);
+        }
     }
 }
