1d3099db538981fac8d42c8accdcf50f5700437c
Menna Emam <menna20101@gmail.com>
2022-12-02 23:24:39 +0200

    P-282A

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/50/A
+//https://codeforces.com/problemset/problem/282/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        byte m = scanner.nextByte();
-        byte n = scanner.nextByte();
-        System.out.println(m * n / 2);
+        short n = scanner.nextShort();
+        short x = 0;
+        scanner.nextLine();
+        for (short i = 0; i < n; i++) {
+            String statement = scanner.nextLine();
+            if(statement.contains("++"))
+                x++;
+            else
+                x--;
+        }
+        System.out.println(x);
     }
 }
