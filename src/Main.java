7ebc5d9deaaf40323f0f5f704959cbdbeb4dc12f
Menna Emam <menna20101@gmail.com>
2022-12-03 01:29:14 +0200

    P-281A

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,12 @@
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-//https://codeforces.com/problemset/problem/339/A
+//https://codeforces.com/problemset/problem/281/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        byte[] numberCount = new byte[3];
-        String orderedString = "";
-
-        String[] string = scanner.nextLine().split("\\+");
-
-        for (byte i = 0; i < string.length; i++)
-            numberCount[Integer.parseInt(string[i])-1]++;
-
-        for (byte i = 0; i < numberCount.length; i++) {
-            for (byte b = 0; b < numberCount[i]; b++) {
-                orderedString += (i+1) + "+";
-            }
-        }
-
-        System.out.println(orderedString.substring(0, orderedString.length() - 1));
+        String string = scanner.nextLine();
+        System.out.println(string.substring(0,1).toUpperCase() + string.substring(1));
     }
 }
