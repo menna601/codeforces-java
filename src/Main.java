4d2994aad72d6c6ec15eace73f8c97f3e14bc770
Menna Emam <menna20101@gmail.com>
2022-12-02 23:07:29 +0200

    P-50A

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,11 @@
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/158/A
+//https://codeforces.com/problemset/problem/50/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        short count = 0;
+        byte m = scanner.nextByte();
         byte n = scanner.nextByte();
-        byte k = scanner.nextByte();
-        byte kthScore=0;
-        for (byte i = 1; i <= n; i++) {
-            byte value = scanner.nextByte();
-            if (value == 0)
-                break;
-            if (i == k) {
-                kthScore = value;
-                count++;
-            }
-            else if (value >= kthScore)
-                count++;
-            else break;
-        }
-        System.out.println(count);
+        System.out.println(m * n / 2);
     }
 }
