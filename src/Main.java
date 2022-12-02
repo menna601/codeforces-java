bb08e75a7d11279b29ed2939c302723c94bbd293
Menna Emam <menna20101@gmail.com>
2022-12-03 01:46:45 +0200

    P-236A

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
-import java.util.Locale;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
-//https://codeforces.com/problemset/problem/281/A
+//https://codeforces.com/problemset/problem/236/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-        System.out.println(string.substring(0,1).toUpperCase() + string.substring(1));
+        boolean[] chars = new boolean[27];
+        byte distinctCharsCount = 0;
+
+        char[] string = scanner.nextLine().toCharArray();
+        for (char c : string) {
+            byte charPosition = (byte) (c - 'a');
+            if(!chars[charPosition]){
+                distinctCharsCount++;
+                chars[charPosition] = true;
+            }
+        }
+        if(distinctCharsCount %2 == 0)
+            System.out.println("CHAT WITH HER!");
+        else
+            System.out.println("IGNORE HIM!");
     }
 }
