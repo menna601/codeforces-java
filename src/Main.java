17b909bc6ead7a407f7be40d0d0d302fc9063cc7
Menna Emam <menna20101@gmail.com>
2022-12-03 01:21:25 +0200

    P-339A

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-//https://codeforces.com/problemset/problem/112/A
+//https://codeforces.com/problemset/problem/339/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String string1 = scanner.nextLine().toLowerCase();
-        String string2 = scanner.nextLine().toLowerCase();
-        int result = string1.compareTo(string2);
-        if(result > 0)
-            result = 1;
-        else if(result < 0)
-            result = -1;
-        System.out.println(result);
+        byte[] numberCount = new byte[3];
+        String orderedString = "";
+
+        String[] string = scanner.nextLine().split("\\+");
+
+        for (byte i = 0; i < string.length; i++)
+            numberCount[Integer.parseInt(string[i])-1]++;
+
+        for (byte i = 0; i < numberCount.length; i++) {
+            for (byte b = 0; b < numberCount[i]; b++) {
+                orderedString += (i+1) + "+";
+            }
+        }
+
+        System.out.println(orderedString.substring(0, orderedString.length() - 1));
     }
 }
