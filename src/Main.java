c6871aaeeff673f6f23180ce8e8f8e276abf8682
Menna Emam <menna20101@gmail.com>
2022-12-23 11:23:28 +0200

    P-677A

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/236/A
+//https://codeforces.com/problemset/problem/677/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean[] chars = new boolean[27];
-        byte distinctCharsCount = 0;
-
-        char[] string = scanner.nextLine().toCharArray();
-        for (char c : string) {
-            byte charPosition = (byte) (c - 'a');
-            if(!chars[charPosition]){
-                distinctCharsCount++;
-                chars[charPosition] = true;
-            }
+        short n = scanner.nextShort();
+        short h = scanner.nextShort();
+        short width = n;
+        for (int i = 0; i < n; i++) {
+            if ( scanner.nextShort() > h )
+                width++;
         }
-        if(distinctCharsCount %2 == 0)
-            System.out.println("CHAT WITH HER!");
-        else
-            System.out.println("IGNORE HIM!");
+        System.out.println(width);
     }
 }
