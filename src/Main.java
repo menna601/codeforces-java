97d05a741b3635ea2c1181674cf621799130c451
Menna Emam <menna20101@gmail.com>
2022-12-23 22:48:57 +0200

    P-427A

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,27 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/381/A
+//https://codeforces.com/problemset/problem/427/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        short n = scanner.nextShort();
-        short[] cards = new short[n];
-        short leftPointer = 0;
-        short rightPointer = (short) (n-1);
-        short maxPerRound = 0;
-        int serejaScore = 0;
-        int dimaScore = 0;
-        boolean isSerejaTurn = true;
+        int n = scanner.nextInt();
+        int untreatedCrimeCount = 0;
+        int policeOfficerCount = 0;
 
-        for (int i = 0; i < cards.length; i++)
-            cards[i] = scanner.nextShort();
-
-        while (leftPointer <= rightPointer) {
-            if (cards[leftPointer] > cards[rightPointer]) {
-                maxPerRound = cards[leftPointer];
-                leftPointer++;
-            } else {
-                maxPerRound = cards[rightPointer];
-                rightPointer--;
+        while (n != 0){
+            byte event = scanner.nextByte();
+            if(event == -1){
+                if (policeOfficerCount != 0)
+                    policeOfficerCount--;
+                else
+                    untreatedCrimeCount++;
+            }else {
+                policeOfficerCount += event;
             }
-            if (isSerejaTurn)
-                serejaScore += maxPerRound;
-            else
-                dimaScore += maxPerRound;
-            isSerejaTurn = !isSerejaTurn;
+            n--;
         }
-
-        System.out.println(serejaScore + " " + dimaScore);
+        System.out.println(untreatedCrimeCount);
     }
 }
