57db5ddb5edb575a5e2c5db32cdd5b243c572739
Menna Emam <menna20101@gmail.com>
2022-12-23 22:02:54 +0200

    P-381A

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,38 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/344/A
+//https://codeforces.com/problemset/problem/381/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int groupCount = 0;
-        byte currentMagnet;
-        byte previousMagnet = 0;
+        short n = scanner.nextShort();
+        short[] cards = new short[n];
+        short leftPointer = 0;
+        short rightPointer = (short) (n-1);
+        short maxPerRound = 0;
+        int serejaScore = 0;
+        int dimaScore = 0;
+        boolean isSerejaTurn = true;
 
-        while (n != 0){
-            currentMagnet = scanner.nextByte();
-            if(previousMagnet != currentMagnet)
-                groupCount++;
-            previousMagnet = currentMagnet;
-            n--;
+        for (int i = 0; i < cards.length; i++)
+            cards[i] = scanner.nextShort();
+
+        while (leftPointer <= rightPointer) {
+            if (cards[leftPointer] > cards[rightPointer]) {
+                maxPerRound = cards[leftPointer];
+                leftPointer++;
+            } else {
+                maxPerRound = cards[rightPointer];
+                rightPointer--;
+            }
+            if (isSerejaTurn)
+                serejaScore += maxPerRound;
+            else
+                dimaScore += maxPerRound;
+            isSerejaTurn = !isSerejaTurn;
         }
-        System.out.println(groupCount);
+
+        System.out.println(serejaScore + " " + dimaScore);
     }
 }
