8e4afd7a0474c0b9f888653a9c98fb6933555303
Menna Emam <menna20101@gmail.com>
2022-12-23 21:20:54 +0200

    P-344A

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,23 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/405/A
+//https://codeforces.com/problemset/problem/344/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        byte noOfCols = scanner.nextByte();
-        byte[] noOfCubesPerCol = new byte[noOfCols];
 
-        for (int i = 0; i < noOfCols; i++) {
-            noOfCubesPerCol[i] = scanner.nextByte();
+        int n = scanner.nextInt();
+        int groupCount = 0;
+        byte currentMagnet;
+        byte previousMagnet = 0;
+
+        while (n != 0){
+            currentMagnet = scanner.nextByte();
+            if(previousMagnet != currentMagnet)
+                groupCount++;
+            previousMagnet = currentMagnet;
+            n--;
         }
-
-        byte[] sortedArray = sortByCounting(noOfCubesPerCol);
-
-        System.out.println(convertArrToString(sortedArray));
-    }
-
-    public static byte[] sortByCounting(byte[] arr){
-        byte max = findMax(arr);
-
-        byte[] count = new byte[max + 1];
-        for (byte num : arr)
-            count[num]++;
-
-        for (byte i = 1; i < count.length; i++) {
-            count[i] += count[i-1];
-        }
-
-        byte[] sorted = new byte[arr.length];
-        for (byte i = 0; i < arr.length; i++) {
-            byte index = (byte)(count[arr[i]] - 1);
-            sorted[index] = arr[i];
-            count[arr[i]]--;
-        }
-
-        return sorted;
-    }
-
-    public static byte findMax(byte[] arr){
-        byte max = 0;
-        for (byte num : arr) {
-            max = (byte) Math.max(max,num);
-        }
-        return max;
-    }
-
-    public static String convertArrToString(byte[] arr){
-        return Arrays.toString(arr).replace(","," ")
-                .replace("[","")
-                .replace("]","");
+        System.out.println(groupCount);
     }
 }
