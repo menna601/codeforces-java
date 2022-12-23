207b41d09a73aff3f1ec81c5f737a2f637e2b8b8
Menna Emam <menna20101@gmail.com>
2022-12-23 20:32:08 +0200

    P-405A

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,54 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/677/A
+//https://codeforces.com/problemset/problem/405/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        short n = scanner.nextShort();
-        short h = scanner.nextShort();
-        short width = n;
-        for (int i = 0; i < n; i++) {
-            if ( scanner.nextShort() > h )
-                width++;
+        byte noOfCols = scanner.nextByte();
+        byte[] noOfCubesPerCol = new byte[noOfCols];
+
+        for (int i = 0; i < noOfCols; i++) {
+            noOfCubesPerCol[i] = scanner.nextByte();
         }
-        System.out.println(width);
+
+        byte[] sortedArray = sortByCounting(noOfCubesPerCol);
+
+        System.out.println(convertArrToString(sortedArray));
+    }
+
+    public static byte[] sortByCounting(byte[] arr){
+        byte max = findMax(arr);
+
+        byte[] count = new byte[max + 1];
+        for (byte num : arr)
+            count[num]++;
+
+        for (byte i = 1; i < count.length; i++) {
+            count[i] += count[i-1];
+        }
+
+        byte[] sorted = new byte[arr.length];
+        for (byte i = 0; i < arr.length; i++) {
+            byte index = (byte)(count[arr[i]] - 1);
+            sorted[index] = arr[i];
+            count[arr[i]]--;
+        }
+
+        return sorted;
+    }
+
+    public static byte findMax(byte[] arr){
+        byte max = 0;
+        for (byte num : arr) {
+            max = (byte) Math.max(max,num);
+        }
+        return max;
+    }
+
+    public static String convertArrToString(byte[] arr){
+        return Arrays.toString(arr).replace(","," ")
+                .replace("[","")
+                .replace("]","");
     }
 }
