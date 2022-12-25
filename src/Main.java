1f2ca2f68d94f591512b3926b947be71fa5b49bd
Menna Emam <menna20101@gmail.com>
2022-12-25 09:15:47 +0200

    P-431A

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/427/A
+//https://codeforces.com/problemset/problem/431/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int untreatedCrimeCount = 0;
-        int policeOfficerCount = 0;
+        short[] stripsCalories = new short[4];
+        int sum = 0;
 
-        while (n != 0){
-            byte event = scanner.nextByte();
-            if(event == -1){
-                if (policeOfficerCount != 0)
-                    policeOfficerCount--;
-                else
-                    untreatedCrimeCount++;
-            }else {
-                policeOfficerCount += event;
-            }
-            n--;
+        for (short i = 0; i < stripsCalories.length; i++)
+            stripsCalories[i] = scanner.nextShort();
+
+        String[] pressSequence = scanner.next().split("");
+        for (String s : pressSequence) {
+            sum += stripsCalories[Integer.parseInt(s) - 1];
         }
-        System.out.println(untreatedCrimeCount);
+
+        System.out.println(sum);
+
     }
 }
