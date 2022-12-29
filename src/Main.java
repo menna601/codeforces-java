ff95fc9cd8eaade3e374147335d08ab0397e9558
Menna Emam <menna20101@gmail.com>
2022-12-29 21:20:09 +0200

    P-268A

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/731/A
+//https://codeforces.com/problemset/problem/268/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        char[] wordChars = scanner.next().toCharArray();
-        char previousChar = 'a';
-        short minRotations = 0;
+        byte teamsCount = scanner.nextByte();
+        byte[] teamsHomeUniform = new byte[101];
+        byte[] teamsGuestUniform = new byte[101];
+        short gamesCount = 0;
 
-        for (byte i = 0; i < wordChars.length; i++) {
-            byte distance = (byte) Math.abs(wordChars[i] - previousChar);
-            if (distance > 13)
-                minRotations += 26 - distance;
-            else
-                minRotations += distance;
-
-            previousChar = wordChars[i];
+        for (int i = 0; i < teamsCount; i++) {
+            teamsHomeUniform[scanner.nextByte()]+=1;
+            teamsGuestUniform[scanner.nextByte()]+=1;
         }
-        System.out.println(minRotations);
+
+        for (int i = 0; i < 101; i++)
+            gamesCount += teamsHomeUniform[i] * teamsGuestUniform[i];
+
+        System.out.println(gamesCount);
     }
 }
