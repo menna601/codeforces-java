965d0a1a81f19674879559fa582433f23df8baa0
Menna Emam <menna20101@gmail.com>
2022-12-29 22:23:46 +0200

    P-228A

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
-//https://codeforces.com/problemset/problem/732/A
+//https://codeforces.com/problemset/problem/228/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        short shovelPrice = scanner.nextShort();
-        byte denominationCoin = scanner.nextByte();
-        byte shovelsCount = 0;
+        Set<Integer> horseShoesColors = new HashSet<>();
 
-        for (byte i = 1; i < 11; i++) {
-            if ((shovelPrice * i) % 10 == 0 || (shovelPrice * i ) % 10 == denominationCoin) {
-                shovelsCount = i;
-                break;
-            }
-        }
+        for (int i = 0; i < 4; i++)
+            horseShoesColors.add(scanner.nextInt());
 
-        System.out.println(shovelsCount);
+        System.out.println(4 - horseShoesColors.size());
     }
 }
