a5c8c1f31a4b239b333271793df0b107f3d07d0c
Menna Emam <menna20101@gmail.com>
2022-12-29 22:13:28 +0200

    P-732A

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
-//https://codeforces.com/problemset/problem/268/A
+//https://codeforces.com/problemset/problem/732/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        byte teamsCount = scanner.nextByte();
-        byte[] teamsHomeUniform = new byte[101];
-        byte[] teamsGuestUniform = new byte[101];
-        short gamesCount = 0;
+        short shovelPrice = scanner.nextShort();
+        byte denominationCoin = scanner.nextByte();
+        byte shovelsCount = 0;
 
-        for (int i = 0; i < teamsCount; i++) {
-            teamsHomeUniform[scanner.nextByte()]+=1;
-            teamsGuestUniform[scanner.nextByte()]+=1;
+        for (byte i = 1; i < 11; i++) {
+            if ((shovelPrice * i) % 10 == 0 || (shovelPrice * i ) % 10 == denominationCoin) {
+                shovelsCount = i;
+                break;
+            }
         }
 
-        for (int i = 0; i < 101; i++)
-            gamesCount += teamsHomeUniform[i] * teamsGuestUniform[i];
-
-        System.out.println(gamesCount);
+        System.out.println(shovelsCount);
     }
 }
