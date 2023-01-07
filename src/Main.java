d193f96be2f8aa4f183a5e2caea94e2f1b118bcb
Menna Emam <menna20101@gmail.com>
2023-01-07 20:04:33 +0200

    P-686A

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-//https://codeforces.com/problemset/problem/443/A
+//https://codeforces.com/problemset/problem/686/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Set<Character> chars = new HashSet<>();
 
-        for (char c : scanner.nextLine().replaceAll("\\}|\\{| |,", "").toCharArray())
-            chars.add(c);
+        short lines = scanner.nextShort();
+        long packsCount = scanner.nextInt();
+        int distressCount = 0;
 
-        System.out.println(chars.size());
+        for (short i = 0; i < lines; i++) {
+            char sign = scanner.next().charAt(0);
+            int value = scanner.nextInt();
+
+            if (sign == '+')
+                packsCount += value;
+            else if (value > packsCount)
+                distressCount++;
+            else
+                packsCount -= value;
+
+        }
+
+        System.out.println("" + packsCount + " " + distressCount);
     }
 }
