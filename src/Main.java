17cd7c4f360657d60e12bfadb0a1816f5cc67ce8
Menna Emam <menna20101@gmail.com>
2023-01-07 20:46:51 +0200

    P-490A

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,36 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
-//https://codeforces.com/problemset/problem/686/A
+//https://codeforces.com/problemset/problem/490/A
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        short lines = scanner.nextShort();
-        long packsCount = scanner.nextInt();
-        int distressCount = 0;
+        List<Short> programmers = new ArrayList<>();
+        List<Short> mathematicians = new ArrayList<>();
+        List<Short> sportsmen = new ArrayList<>();
+        short children = scanner.nextShort();
 
-        for (short i = 0; i < lines; i++) {
-            char sign = scanner.next().charAt(0);
-            int value = scanner.nextInt();
+        for (short i = 1; i <= children; i++)
+            switch (scanner.nextByte()) {
+                case 1:
+                    programmers.add(i);
+                    break;
+                case 2:
+                    mathematicians.add(i);
+                    break;
+                case 3:
+                    sportsmen.add(i);
+                    break;
+            }
+        short pmMin = (short) Math.min(programmers.size(), mathematicians.size());
+        short maxTeamCount = (short) Math.min(pmMin, sportsmen.size());
 
-            if (sign == '+')
-                packsCount += value;
-            else if (value > packsCount)
-                distressCount++;
-            else
-                packsCount -= value;
+        System.out.println(maxTeamCount);
 
+        for (short i = 0; i < maxTeamCount; i++) {
+            System.out.print(programmers.get(i) + " ");
+            System.out.print(mathematicians.get(i) + " ");
+            System.out.println(sportsmen.get(i));
         }
-
-        System.out.println("" + packsCount + " " + distressCount);
     }
 }
